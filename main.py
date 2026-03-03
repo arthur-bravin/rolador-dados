74fa9555405377bbbc778af7164be47da2affc85
arthur-bravin <arthurbravin29@gmail.com>
2026-03-03 07:08:36 -0300

    alteração no funcionamento das rolagens e alteração de layout

--- main.py
+++ main.py
@@ -7,127 +7,228 @@ def main(page: ft.Page):
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     
     # Campo de texto que exibirá o resultado da rolagem dos dados
-    txt_result = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
+    txt_result = ft.Text(value="0", size=60, text_align=ft.TextAlign.CENTER, width=100, disabled=True)
+    # Campo de texto que exibirá o resultado detalhado da rolagem dos dados
+    txt_result_detail = ft.Text(value="", size=14, text_align=ft.TextAlign.CENTER, width=500, disabled=True, visible=False)
 
     # Campo de texto que mostrará a quantidade de dados à ser rolada
-    txt_number = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
+    txt_quantity_d4 = ft.TextField(label="D4", text_size=30, value="0", width=60, text_align=ft.TextAlign.CENTER, read_only=True)
+    txt_quantity_d6 = ft.TextField(label="D6", text_size=30, value="0", width=60, text_align=ft.TextAlign.CENTER, read_only=True)
+    txt_quantity_d8 = ft.TextField(label="D8", text_size=30, value="0", width=60, text_align=ft.TextAlign.CENTER, read_only=True)
+    txt_quantity_d10 = ft.TextField(label="D10", text_size=30, value="0", width=60, text_align=ft.TextAlign.CENTER, read_only=True)
+    txt_quantity_d12 = ft.TextField(label="D12", text_size=30, value="0", width=60, text_align=ft.TextAlign.CENTER, read_only=True)
+    txt_quantity_d20 = ft.TextField(label="D20", text_size=30, value="0", width=60, text_align=ft.TextAlign.CENTER, read_only=True)
 
     # Campo de texto que mostrará as rolagens realizadas
     txt_log = ft.TextField(label="Registro de Rolagens", multiline=True, min_lines=5, max_lines=5, read_only=True)
     
     # Função para subtrair
-    def minus_click(e):
-        reset_values(0)
+    def minus_click(e: ft.TextButton):
+        change_values(e, 0)
 
     # Função para somar
-    def plus_click(e):
-        reset_values(1)
-
-    # Função de rolagem dos dados
-    def dice_click(e):
-        if int(txt_number.value) < 1:
-            pass
-        else:            
-            dice_sides = switch_case_dice(e.control.content)
-            dice_quantity = int(txt_number.value)
-            roll_count = dice_quantity
-            dice_result = 0
-            txt_results = ""
-
-            while roll_count > 0:
-                roll = rd.randint(1, dice_sides)
-                dice_result = dice_result + roll
-                roll_count = roll_count - 1               
-                txt_results = f"{roll}" if txt_results == "" else txt_results + f" + {roll}"
-
-            if dice_result > 0:
-                if dice_result == 1:
-                    txt_result.color = ft.Colors.RED
-                elif dice_result == dice_sides:
-                    txt_result.color =ft.Colors.GREEN
-
-                txt_result.value = str(dice_result)
-                
-            if txt_log.value == "":
-                txt_log.value = f"{dice_quantity}{e.control.content} = {str(dice_result)}" if dice_quantity == 1 else f"{dice_quantity}{e.control.content} = ({txt_results}) {str(dice_result)}"
-            else:
-                txt_log.value = f"{dice_quantity}{e.control.content} = {str(dice_result)}" + f"\n{txt_log.value}" if dice_quantity == 1 else f"{dice_quantity}{e.control.content} = ({txt_results}) {str(dice_result)}" + f"\n{txt_log.value}"
-
-            txt_number.value = "0"
-            page.update()
-        
-    # FUnção para determinar o número de lados de acordo com o dado rolado
-    def switch_case_dice(dice_name):
-        match dice_name:
-            case "d4": return 4
-            case "d6": return 6
-            case "d8": return 8
-            case "d10": return 10
-            case "d12": return 12
-            case "d20": return 20
-            case "d100": return 100
+    def plus_click(e: ft.TextButton):
+        change_values(e, 1)
     
+    def change_values(e: ft.TextButton, button):
+        # 0 = Botão sinal de "-"
+        # 1 = Botão sinal de "+"
+        txt_result.value = str(0)
+        txt_result.color = None
+        txt_result_detail.visible = False
+
+        if button == 0:  
+            match e.control.key:
+                case "R4": txt_quantity_d4.value = check_value(str(int(txt_quantity_d4.value) - 1))
+                case "R6": txt_quantity_d6.value = check_value(str(int(txt_quantity_d6.value) - 1))
+                case "R8": txt_quantity_d8.value = check_value(str(int(txt_quantity_d8.value) - 1))
+                case "R10": txt_quantity_d10.value = check_value(str(int(txt_quantity_d10.value) - 1))
+                case "R12": txt_quantity_d12.value = check_value(str(int(txt_quantity_d12.value) - 1))
+                case "R20": txt_quantity_d20.value = check_value(str(int(txt_quantity_d20.value) - 1))
+        else:
+            match e.control.key:
+                case "A4": txt_quantity_d4.value = check_value(str(int(txt_quantity_d4.value) + 1))
+                case "A6": txt_quantity_d6.value = check_value(str(int(txt_quantity_d6.value) + 1))
+                case "A8": txt_quantity_d8.value = check_value(str(int(txt_quantity_d8.value) + 1))
+                case "A10": txt_quantity_d10.value = check_value(str(int(txt_quantity_d10.value) + 1))
+                case "A12": txt_quantity_d12.value = check_value(str(int(txt_quantity_d12.value) + 1))
+                case "A20": txt_quantity_d20.value = check_value(str(int(txt_quantity_d20.value) + 1))
+
+        page.update() # Atualiza a interface
+    
+    def reset_values():
+        txt_quantity_d4.value = "0"
+        txt_quantity_d6.value = "0"
+        txt_quantity_d8.value = "0"
+        txt_quantity_d10.value = "0"
+        txt_quantity_d12.value = "0"
+        txt_quantity_d20.value = "0"
+
     # Função para verificar o valor da quantidade selecionada
     # Para que não seja possível escolher um valor negativo
     def check_value(value):
         if value and int(value) < 0:
             value = "0"
         return value
-    
-    def reset_values(button):
-        # 0 = Botão sinal de "-"
-        # 1 = Botão sinal de "+"
-        if button == 0:            
-            txt_result.value = str(0)
-            txt_result.color = None
-            txt_number.value = check_value(str(int(txt_number.value) - 1))
-            page.update() # Atualiza a interface
-        else:
-            txt_result.value = str(0)
-            txt_result.color = None
-            txt_number.value = check_value(str(int(txt_number.value) + 1))
-            page.update()
-    
-    # Limpa o log de rolagens
-    def clear_rolls_log():
-        txt_result.value = str(0)
-        txt_number.value = str(0)
-        txt_log.value = ""
-        page.update()
 
+    def execute_roll():
+        roll_result = 0
+        roll_sum = 0
+        roll_detail = ""
+        roll_count = 0
+        final_result = 0
+        final_detail = ""
+
+        if int(txt_quantity_d4.value) > 0:
+            roll_count = int(txt_quantity_d4.value)
+            while roll_count > 0:
+                roll_result = rd.randint(1, 4)
+                roll_sum = roll_sum + roll_result
+                roll_detail = f"{roll_result}" if roll_detail == "" else roll_detail + f" + {roll_result}"
+                roll_count = roll_count -1
+
+            final_result = final_result + roll_sum
+            final_detail = f"D4({roll_detail})"
+
+        roll_result = 0
+        roll_sum = 0
+        roll_detail = ""
+
+        if int(txt_quantity_d6.value) > 0:
+            roll_count = int(txt_quantity_d6.value)
+            while roll_count > 0:
+                roll_result = rd.randint(1, 6)
+                roll_sum = roll_sum + roll_result
+                roll_detail = f"{roll_result}" if roll_detail == "" else roll_detail + f" + {roll_result}"
+                roll_count = roll_count -1
+
+            final_result = final_result + roll_sum
+            final_detail = final_detail + f"D6({roll_detail})" if final_detail == "" else final_detail + f" + D6({roll_detail})"
+
+        roll_result = 0
+        roll_sum = 0
+        roll_detail = ""
+
+        if int(txt_quantity_d8.value) > 0:
+            roll_count = int(txt_quantity_d8.value)
+            while roll_count > 0:
+                roll_result = rd.randint(1, 8)
+                roll_sum = roll_sum + roll_result
+                roll_detail = f"{roll_result}" if roll_detail == "" else roll_detail + f" + {roll_result}"
+                roll_count = roll_count -1
+
+            final_result = final_result + roll_sum
+            final_detail = final_detail + f"D8({roll_detail})" if final_detail == "" else final_detail + f" + D8({roll_detail})"
+
+        roll_result = 0
+        roll_sum = 0
+        roll_detail = ""
+
+        if int(txt_quantity_d10.value) > 0:
+            roll_count = int(txt_quantity_d10.value)
+            while roll_count > 0:
+                roll_result = rd.randint(1, 10)
+                roll_sum = roll_sum + roll_result
+                roll_detail = f"{roll_result}" if roll_detail == "" else roll_detail + f" + {roll_result}"
+                roll_count = roll_count -1
+
+            final_result = final_result + roll_sum
+            final_detail = final_detail + f"D10({roll_detail})" if final_detail == "" else final_detail + f" + D10({roll_detail})"
+
+        roll_result = 0
+        roll_sum = 0
+        roll_detail = ""
+
+        if int(txt_quantity_d12.value) > 0:
+            roll_count = int(txt_quantity_d12.value)
+            while roll_count > 0:
+                roll_result = rd.randint(1, 12)
+                roll_sum = roll_sum + roll_result
+                roll_detail = f"{roll_result}" if roll_detail == "" else roll_detail + f" + {roll_result}"
+                roll_count = roll_count -1
+
+            final_result = final_result + roll_sum
+            final_detail = final_detail + f"D12({roll_detail})" if final_detail == "" else final_detail + f" + D12({roll_detail})"
+
+        roll_result = 0
+        roll_sum = 0
+        roll_detail = ""
+
+        if int(txt_quantity_d20.value) > 0:
+            roll_count = int(txt_quantity_d20.value)
+            while roll_count > 0:
+                roll_result = rd.randint(1, 20)
+                roll_sum = roll_sum + roll_result
+                roll_detail = f"{roll_result}" if roll_detail == "" else roll_detail + f" + {roll_result}"
+                roll_count = roll_count -1
+
+            final_result = final_result + roll_sum
+            final_detail = final_detail + f"D20({roll_detail})" if final_detail == "" else final_detail + f" + D20({roll_detail})"
+
+        final_detail = f"{final_detail} = {final_result}"
+        txt_result.value = final_result
+        txt_result_detail.value = final_detail
+        txt_result_detail.visible = True
+
+        reset_values()
 
     # Adiciona os elementos na página
     page.add(
         ft.Row(
             [
-                txt_result
+                txt_result,
             ],
             alignment=ft.MainAxisAlignment.CENTER,
         ),
         ft.Row(
             [
-                ft.IconButton(ft.Icons.REMOVE, on_click=minus_click),
-                txt_number,
-                ft.IconButton(ft.Icons.ADD, on_click=plus_click),
+                txt_result_detail,
             ],
             alignment=ft.MainAxisAlignment.CENTER,
         ),
+        ft.Row([],alignment=ft.MainAxisAlignment.CENTER,),
+        ft.Row([],alignment=ft.MainAxisAlignment.CENTER,),
+        ft.Row([],alignment=ft.MainAxisAlignment.CENTER,),
+        ft.Row([],alignment=ft.MainAxisAlignment.CENTER,),
+        ft.Row([],alignment=ft.MainAxisAlignment.CENTER,),
+        ft.Row([],alignment=ft.MainAxisAlignment.CENTER,),
         ft.Row(
             [
-                ft.TextButton(content="d4", style=ft.ButtonStyle(color=ft.Colors.WHITE, bgcolor=ft.Colors.LIGHT_BLUE), on_click=dice_click),
-                ft.TextButton(content="d6", style=ft.ButtonStyle(color=ft.Colors.WHITE, bgcolor=ft.Colors.LIGHT_BLUE), on_click=dice_click),
-                ft.TextButton(content="d8", style=ft.ButtonStyle(color=ft.Colors.WHITE, bgcolor=ft.Colors.LIGHT_BLUE), on_click=dice_click),
-                ft.TextButton(content="d10", style=ft.ButtonStyle(color=ft.Colors.WHITE, bgcolor=ft.Colors.LIGHT_BLUE), on_click=dice_click),
-                ft.TextButton(content="d12", style=ft.ButtonStyle(color=ft.Colors.WHITE, bgcolor=ft.Colors.LIGHT_BLUE), on_click=dice_click),
-                ft.TextButton(content="d20", style=ft.ButtonStyle(color=ft.Colors.WHITE, bgcolor=ft.Colors.LIGHT_BLUE), on_click=dice_click),
-                ft.TextButton(content="d100", style=ft.ButtonStyle(color=ft.Colors.WHITE, bgcolor=ft.Colors.LIGHT_BLUE), on_click=dice_click),
+                ft.TextButton(key="R4", content="-", on_click=minus_click),
+                txt_quantity_d4,
+                ft.TextButton(key="A4", content="+", on_click=plus_click),
+                ft.TextButton(key="R6", content="-", on_click=minus_click),
+                txt_quantity_d6,
+                ft.TextButton(key="A6", content="+", on_click=plus_click),
+                ft.TextButton(key="R8", content="-", on_click=minus_click),
+                txt_quantity_d8,
+                ft.TextButton(key="A8", content="+", on_click=plus_click),
             ],
             alignment=ft.MainAxisAlignment.CENTER,
         ),
+        ft.Row([],alignment=ft.MainAxisAlignment.CENTER,),
+        ft.Row([],alignment=ft.MainAxisAlignment.CENTER,),
+        ft.Row([],alignment=ft.MainAxisAlignment.CENTER,),
         ft.Row(
             [
-                txt_log,
-                ft.TextButton(content="Limpar Rolagens", on_click=clear_rolls_log),
+                ft.TextButton(key="R10", content="-", on_click=minus_click),
+                txt_quantity_d10,
+                ft.TextButton(key="A10", content="+", on_click=plus_click),
+                ft.TextButton(key="R12", content="-", on_click=minus_click),
+                txt_quantity_d12,
+                ft.TextButton(key="A12", content="+", on_click=plus_click),
+                ft.TextButton(key="R20", content="-", on_click=minus_click),
+                txt_quantity_d20,
+                ft.TextButton(key="A20", content="+", on_click=plus_click),
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
+        ),
+        ft.Row([],alignment=ft.MainAxisAlignment.CENTER,),
+        ft.Row([],alignment=ft.MainAxisAlignment.CENTER,),
+        ft.Row([],alignment=ft.MainAxisAlignment.CENTER,),
+        ft.Row(
+            [
+                ft.TextButton(content="Rolar", width=150, height=60, style=ft.ButtonStyle(bgcolor=ft.Colors.LIGHT_BLUE, color=ft.Colors.WHITE, padding=ft.Padding.all(20)), on_click=execute_roll),
             ],
             alignment=ft.MainAxisAlignment.CENTER,
         )
